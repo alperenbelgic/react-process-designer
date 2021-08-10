78e770f4b730574a45db35b85f2df359de243297
Alperen Belgic (CA Tech) <alperenbelgic@outlook.com>
2021-08-10 19:13:52 +0100

    initial designer container hierarchy created.

---
+++ src/components/designer/Designer.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import './Designer.css'
+
+
+export function Designer() {
+
+  return (
+    
+<div className="container">
+
+<div className="header">
+  header
+</div>
+
+<div className="page-content">
+
+  <div className="left-menu">
+    left menu
+  </div>
+  <div className="content">
+    <div className="designer-container">
+
+      <div className="item item1">
+        item 1
+      </div>
+
+      <div className="item">
+        item 2
+      </div>
+    </div>
+  </div>
+
+</div>
+
+</div>
+
+
+  );
+}
